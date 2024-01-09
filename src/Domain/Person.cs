c630701477547484e8f5a2659a3f59f8c15ec61c
Bram Van Coile <bram.vancoile@student.hogent.be>
2024-01-09 22:52:54 +0100

    Add Domain Classlib

---
+++ src/Domain/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain;
+using System;
+
+public class Person
+{
+    public Person()
+    {
+
+    }
+    String name = "Bram";
+
+    public fun SayHello()
+    {
+        System.Console.WriteLine($"Hello, {name}");
+    }
+}
+
